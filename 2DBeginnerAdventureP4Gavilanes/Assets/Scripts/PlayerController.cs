c27e608e56598cd23a7de5ad70753cea1bd8d13e
Giannamia Gavilanes <719613@student.dusd.net>
2024-10-22 11:46:57 -0700

    Made more tiles and put character movement
    I put more tiles for my world's environment, and got my character to be able to move vertically and horizontally.

--- 2DBeginnerAdventureP4Gavilanes/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Gavilanes/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,9 +14,33 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+        
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
+
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f; 
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 1.0f * vertical;
         transform.position = position;
     }
 }
