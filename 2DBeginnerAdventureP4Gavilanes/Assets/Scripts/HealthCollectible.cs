70e0c012163c28a73aaf741dcc6f676927232c09
Giannamia Gavilanes <719613@student.dusd.net>
2024-11-07 11:47:17 -0800

    Created scripts
    I created scripts for my player, collectibles and to create boundaries.

---
+++ 2DBeginnerAdventureP4Gavilanes/Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        BetterPlayerController controller = other.GetComponent<BetterPlayerController>();
+        if (controller != null)
+        {
+            if(controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
