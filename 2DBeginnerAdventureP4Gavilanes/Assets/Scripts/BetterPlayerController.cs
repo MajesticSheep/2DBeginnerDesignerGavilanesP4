215f0505ef514a5354e4d8ce546a9c5660868d11
Giannamia Gavilanes <719613@student.dusd.net>
2024-11-12 11:50:57 -0800

    Created animations
    I put animations for my characters when they move or get hit.

--- 2DBeginnerAdventureP4Gavilanes/Assets/Scripts/BetterPlayerController.cs
+++ 2DBeginnerAdventureP4Gavilanes/Assets/Scripts/BetterPlayerController.cs
@@ -8,7 +8,7 @@ public class BetterPlayerController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -19,11 +19,15 @@ public class BetterPlayerController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         
     }
 
@@ -32,6 +36,17 @@ public class BetterPlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if(isInvincible)
         {
@@ -44,6 +59,7 @@ public class BetterPlayerController : MonoBehaviour
 
     }
 
+    //This is so silly, goober, sigma :,)
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -54,6 +70,8 @@ public class BetterPlayerController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        animator.SetTrigger("Hit");
+
         if(amount < 0)
         {
             if(isInvincible)
